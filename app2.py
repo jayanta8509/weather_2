1fa6d1d6a83815fd6c2fd5f23a62536966220535
jayanta8509 <jayantameslova@gmail.com>
2024-01-01 00:57:54 +0530

    commit

---
+++ app2.py
@@ -0,0 +1,35 @@
+import streamlit as st
+import requests
+from bs4 import BeautifulSoup
+
+def get_weather_info(city):
+    # Perform a Google search for the weather
+    search_query = f"Weather in {city}"
+    google_url = f"https://www.google.com/search?q={search_query}"
+    response = requests.get(google_url)
+
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        # Extract the weather information from the search results
+        weather_info_tag = soup.find("div", {"class": "BNeawe iBp4i AP7Wnd"})
+        
+        if weather_info_tag is not None:
+            weather_info = weather_info_tag.get_text()
+            
+            if weather_info:
+                return weather_info
+            else:
+                return "No weather information found."
+        else:
+            return "Weather information not found on the page."
+    else:
+        return "Failed to retrieve weather information. Please try again."
+
+# Streamlit UI
+st.title("Google Weather App")
+
+city = st.text_input("Enter the city:", "New York")
+if st.button("Get Weather"):
+    weather_result = get_weather_info(city)
+    st.success(f"The weather in {city} is {weather_result}")
